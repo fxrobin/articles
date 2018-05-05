26db018e08221d6260042b6d35f3804add7456a3
FX Robin <fxrobin@gmail.com>
2018-05-05 15:01:28 +0200

    sources du projet associé à l'article sur les préconditions.

---
+++ preconditions/src/test/java/fr/fxjavadevblog/SimpleTest.java
@@ -0,0 +1,15 @@
+package fr.fxjavadevblog;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class SimpleTest {
+
+	@Test
+	void test() {
+		log.info("Ok");
+	}
+
+}
